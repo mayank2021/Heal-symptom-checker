443bfcfdf253a7da654e5fccf1f55f6b82743ff2
mayankSonkar2@14 <55187056+Mayank1-cpu@users.noreply.github.com>
2022-02-13 02:28:17 +0530

    remove comments

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import { Route, Routes } from "react-router-dom";
 import Sawo from "./components/Sawo/Sawo";
 import Home from "./Pages/Home/Home";
 import Convo from "./Pages/Convo/Convo";
-import Chat from "./Chat";
 
 function App() {
   return (
@@ -12,15 +11,7 @@ function App() {
     <Route path="/" element={<Sawo/>} />
     <Route path="/home" element={<Home/>} />
     <Route path="/convo" element={<Convo/>} />
-    <Route path="/chat" element={<Chat/>} />
     </Routes>
-      // {/* <button class="button" id="runButton" onClick={run}>
-      //   Run <div class="button-bg"></div>
-      // </button> */}
-      // <Home/>
-      // <Sawo/>
-      // {/* <Chat/> */}
-
   );
 }
 
