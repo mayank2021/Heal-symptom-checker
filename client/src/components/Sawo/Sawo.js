f266585be5f2a292b38c3bffb8c0d6553d5f3d24
aditi944 <aditijha201200@gmail.com>
2022-02-12 21:19:27 +0530

    sawo

--- client/src/components/Sawo/Sawo.js
+++ client/src/components/Sawo/Sawo.js
@@ -5,9 +5,9 @@ const LoginPage = () => {
     useEffect(() => {
         var config = {
             // should be same as the id of the container created on 3rd step
-            containerID: '<container-ID>',
+            containerID: 'sawo-container',
             // can be one of 'email' or 'phone_number_sms'
-            identifierType: 'phone_number_sms',
+            identifierType: 'email',
             // Add the API key copied from 5th step
             apiKey: '742e2c57-726a-4c16-a6ca-ef4e4101c4d5',
             // Add a callback here to handle the payload sent by sdk
