04b437dcf9a2533e4345ec231fac1f6678131867
mayankSonkar2@14 <55187056+Mayank1-cpu@users.noreply.github.com>
2022-02-13 14:26:15 +0530

    update result page logic

--- client/src/Pages/Convo/Convo.js
+++ client/src/Pages/Convo/Convo.js
@@ -6,6 +6,7 @@ import io from "socket.io-client";
 import * as SIP from "sip.js";
 import { useEffect, useState } from "react";
 import Ship from "../../Images/start-up.png";
+import Result from "../Result/Result";
 //Original
 const socket = io.connect("http://localhost:8000");
 
@@ -83,6 +84,10 @@ const Convo = () => {
 
   const [scroll, setScroll] = useState(false);
   const [dialogue, setDialogue] = useState([]);
+  const [age, setAge] = useState('');
+  const [gender, setGender] = useState('');
+  const [end, setEnd] = useState(false);
+  const [cause, setCause] = useState('');
 
   useEffect(() => {
     window.onscroll = () => {
@@ -92,12 +97,28 @@ const Convo = () => {
       var element = document.getElementById("convoChatScroll");
       element.scrollTop = element.scrollHeight;
     }
+
+     if(dialogue.length >= 1){
+    dialogue.map((d) => {
+      if(d.speaker === 3 ) localStorage.setItem('sex', d.conversation );
+      else if(d.speaker === 4) localStorage.setItem('age', d.conversation );
+      else if(d.speaker === 5) localStorage.setItem('cause', d.conversation );
+      else if(d.speaker === 6) setEnd(true);
+    })
+  }
+   
   }, [dialogue]);
+ 
+
+  console.log(dialogue);
+  if(end){
+    window.location = 'http://localhost:3000/result';
+  }
 
   return (
     <>
       <Nav />
-      <div className="convo-main-container">
+        <div className="convo-main-container">
         <button className="utility--button convo-btn--one" onClick={run}>
           start diagnosis
           <img src={Ship} alt="start" />
@@ -107,17 +128,16 @@ const Convo = () => {
           <img src={Ship} alt="stop" />
         </button>
         <div id="convoChatScroll" className="convo-chat--container">
-          {dialogue.length >= 1 &&
-            dialogue.map((d) => {
+          {dialogue.length >= 1 && 
+            dialogue.map((d,ind) => {
               return (
                 <div
-                  key={d.id}
-                  className={`convo-message--contaner ${
-                    d.speaker !== 0 ? "message-two" : null
-                  }`}
+                  key={ind}
+                  style={{display:`${(d.speaker === 3 || d.speaker === 4 || d.speaker === 6 || d.speaker === 6)?'none':null}`}}
+                  className={`convo-message--contaner ${d.speaker === 1 && "message-two"}`}
                 >
-                  <h3>{d.speaker === 0 ? "Dasha" : "You"}</h3>
-                  <p>{d.conversation}</p>
+                  <h3>{(d.speaker === 0 && "Dasha") || (d.speaker === 1 && "You")}</h3>
+                  <p>{( d.speaker === 0 || d.speaker === 1) && d.conversation}</p>
                 </div>
               );
             })}
--- client/src/Pages/Result/Result.js
+++ client/src/Pages/Result/Result.js
@@ -9,57 +9,24 @@ import Sex from '../../Images/sex.png';
 import Age from '../../Images/age.png';
 
 function Result() {
+
+  const sex = localStorage.getItem('sex');
+  const age = localStorage.getItem('age');
+  const cause = localStorage.getItem('cause');
+
   return (
     <>
     <Nav/>
     <div className='result-main--container'>
-      <h1>Summary</h1>
+      <h1>Conclusion</h1>
      <div className='result-top--container'>
       <div className='basic-info'>
-         <Card img={Age} title="Age" value="30"/>
-         <Card img={Sex} title="Sex" value="Male"/>
-         <Card img={Height} title="Height" value="5'10''"/>
-         <Card img={Weight} title="Weight" value="70 Kg"/>
+         <Card img={Age} title="Age" value={age}/>
+         <Card img={Sex} title="Sex" value={sex}/>
       </div>
      </div>
      <div className='result-bottom--container'>
-      <div className='result-bmi-container'>
-        <div className='bmi-element--container utility-bold'>
-          <p>BMI</p>
-          <span>Weight</span>
-        </div>
-        <div  className='bmi-element--container'>
-          <p>Below 18.5</p>
-          <span>Under weight</span>
-        </div>
-        <div  className='bmi-element--container'>
-          <p>18.5 - 24.9</p>
-          <span>Normal weight</span>
-        </div>
-        <div  className='bmi-element--container'>
-          <p>25.0 - 29.9</p>
-          <span>Over weight</span>
-        </div>
-        <div  className='bmi-element--container'>
-          <p>30.0 - 34.9</p>
-          <span>Obesity class 1</span>
-        </div>
-        <div  className='bmi-element--container'>
-          <p>35.0 - 39.9</p>
-          <span>Obesity class 2</span>
-        </div>
-        <div  className='bmi-element--container'>
-          <p>Above 40</p>
-          <span>Obesity class 4</span>
-        </div>
-        <div  className='bmi-element--container utility-bold'>
-          <p>Your BMI</p>
-          <span>45</span>
-        </div>
-      </div>
-      <div className='result-chart--container'>
-           <img src="https://lh3.googleusercontent.com/g0Jw-I6-gH2DVCpnl3u8QKZVT_meR9lcJlpyeSZ-MyvwLnyEZvgyrY5frldA8HCv55s=w280-rwa" alt="alt"/>
-      </div>
+      Possible cause:<span>{cause}</span>
      </div>
     </div>
     <Footer/>
@@ -68,3 +35,41 @@ function Result() {
 }
 
 export default Result;
+
+// <div className='result-bmi-container'>
+//         <div className='bmi-element--container utility-bold'>
+//           <p>BMI</p>
+//           <span>Weight</span>
+//         </div>
+//         <div  className='bmi-element--container'>
+//           <p>Below 18.5</p>
+//           <span>Under weight</span>
+//         </div>
+//         <div  className='bmi-element--container'>
+//           <p>18.5 - 24.9</p>
+//           <span>Normal weight</span>
+//         </div>
+//         <div  className='bmi-element--container'>
+//           <p>25.0 - 29.9</p>
+//           <span>Over weight</span>
+//         </div>
+//         <div  className='bmi-element--container'>
+//           <p>30.0 - 34.9</p>
+//           <span>Obesity class 1</span>
+//         </div>
+//         <div  className='bmi-element--container'>
+//           <p>35.0 - 39.9</p>
+//           <span>Obesity class 2</span>
+//         </div>
+//         <div  className='bmi-element--container'>
+//           <p>Above 40</p>
+//           <span>Obesity class 4</span>
+//         </div>
+//         <div  className='bmi-element--container utility-bold'>
+//           <p>Your BMI</p>
+//           <span>45</span>
+//         </div>
+//       </div>
+//       <div className='result-chart--container'>
+//            <img src="https://lh3.googleusercontent.com/g0Jw-I6-gH2DVCpnl3u8QKZVT_meR9lcJlpyeSZ-MyvwLnyEZvgyrY5frldA8HCv55s=w280-rwa" alt="alt"/>
+//       </div>
